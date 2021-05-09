002d5498ec99af1c0f48b9faefe576c83722d143
GeorgeenaJoseph <74125285+GeorgeenaJoseph@users.noreply.github.com>
2021-05-09 13:42:59 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,46 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+         
+          restitution :0.4,
+          friction :0.0,
+          
+          //isStatic:true
+         
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.visibility=225;
+      World.add(world, this.body);
+    }
+    display(){
+      var angle = this.body.angle;
+      var pos= this.body.position;
+
+      console.log(this.body.speed)
+
+      if(this.body.speed<6)
+      {
+      console.log(this.body.speed)
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0,0,this.width, this.height);
+      pop();
+    
+      }
+      else
+      {
+        
+        
+        push();
+        this.visibility=this.visibility-5;
+        pop();
+        World.remove(world, this.body);
+
+        
+      }
+      }
+      }
